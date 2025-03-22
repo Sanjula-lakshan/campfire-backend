5666072d783d5923004844d11bbbf48fb085b135
Sanjula-lakshan <sanjularathnamalala2001@gmail.com>
2025-03-22 12:49:56 +0530

    Create Database Models

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    default: 'user'
+  },
+  imageUrl: {
+    type: String,
+    default: ''
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
